18f02020b49371b75cdccec7643d533c46d76db2
Noa <noait2612@gmail.com>
2025-10-23 23:01:28 +0300

    started to work in Measure Theory ex01 and added ex01 and lab01 in intro

---
+++ Year 3/Introduction To Computer Science/Assignments/Ex01/additional_file.py
@@ -0,0 +1,13 @@
+#################################################################
+# FILE : math_print.py
+# WRITER : your_name , your_login , your_id
+# EXERCISE : intro2cs ex1 2025
+# DESCRIPTION: A simple program that provides my secret key.
+# STUDENTS I DISCUSSED THE EXERCISE WITH: -
+# WEB PAGES I USED: w3schools
+# NOTES: ...
+#################################################################
+#Function of secret key hard-coded.
+def secret_function():
+    """this is a function that prints my secret key"""
+    print("My username is 'your_login' and I found the string 'UtS1uCU2t4on' in the results file.")
