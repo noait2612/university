8a66cc82755953a3b9cb09d11fbcc35c2ee447dd
Noa <noait2612@gmail.com>
2025-10-28 21:11:11 +0200

    ex02 in intro beautify take2

--- Year 3/Introduction To Computer Science/Assignments/Ex02/largest_and_smallest.py
+++ Year 3/Introduction To Computer Science/Assignments/Ex02/largest_and_smallest.py
@@ -27,25 +27,13 @@ def largest_and_smallest(first_number, second_number, third_number):
 
 
 def check_largest_and_smallest():
-    any_false_calculation = False
-    if not scenario_is_correct_largest_and_smallest(17, 1, 16, 17, 1):
-        any_false_calculation = True
-
-    if not scenario_is_correct_largest_and_smallest(1, 17, 6, 17, 1):
-        any_false_calculation = True
-
-    if not scenario_is_correct_largest_and_smallest(1, 1, 2, 2, 1):
-        any_false_calculation = True
-
-    # Checking Negative numbers.
-    if not scenario_is_correct_largest_and_smallest(-1, 0, -3, 0, -3):
-        any_false_calculation = True
-
-    # Checking decimals
-    if not scenario_is_correct_largest_and_smallest(2.25, 2.5, 2.75, 2.75, 2.25):
-        any_false_calculation = True
-
-    return not any_false_calculation
+    """Used Sugar Syntax instead of multiple if's for each case since is the same as a = a & True"""
+    predicate = scenario_is_correct_largest_and_smallest(17, 1, 16, 17, 1)
+    predicate &= scenario_is_correct_largest_and_smallest(1, 17, 6, 17, 1)
+    predicate &= scenario_is_correct_largest_and_smallest(1, 1, 2, 2, 1)
+    predicate &= scenario_is_correct_largest_and_smallest(-1, 0, -3, 0, -3)
+    predicate &= scenario_is_correct_largest_and_smallest(2.25, 2.5, 2.75, 2.75, 2.25)
+    return predicate
 
 
 # Single Scenario check for largest and smallest (to reduce duplications).
