bc5de312af5de8336736d085394295d00050af88
Noa <noait2612@gmail.com>
2025-10-28 20:54:44 +0200

    ex02 in intro beautify

--- Year 3/Introduction To Computer Science/Assignments/Ex02/calculate_mathematical_expression.py
+++ Year 3/Introduction To Computer Science/Assignments/Ex02/calculate_mathematical_expression.py
@@ -7,25 +7,32 @@
 # WEB PAGES I USED: w3schools, docs.python.org
 # NOTES: ...
 #################################################################
-#Gets two numbers and one mathematical operation and returns the mathematical operation result on them.
+# Gets two numbers and one mathematical operation and returns the mathematical operation result on them.
 def calculate_mathematical_expression(first_number, second_number, operation):
-    if operation == "+":
-        return first_number + second_number
-    if operation == "*":
-        return first_number * second_number
-    if operation == "-":
-        return first_number-second_number
-    if operation == ":" and second_number != 0:
-        return first_number/second_number
-    #This else case catches not allowed operation or allowed operation and invalid action (like diving zero).
-    else:
-        return None
+    match operation:
+        case "+":
+            return first_number + second_number
+        case "*":
+            return first_number * second_number
+        case "-":
+            return first_number - second_number
+        case ":":
+            if second_number != 0:
+                return first_number / second_number
+            else:
+                return None
+        case _:
+            return None
+    # This else case catches not allowed operation or allowed operation and invalid action (like diving zero).
 
 
 """Gets an expression from the format 'number operation number', split by format
  and return using calculate_mathematical_expression the operation result"""
+
+
 def calculate_from_string(expression):
-        split_expression = expression.split(' ')
-        if len(split_expression) != 3:
-            return None
-        return calculate_mathematical_expression(split_expression[0], split_expression[1], split_expression[2])
+    split_expression = expression.split(' ')
+    if len(split_expression) != 3:
+        return None
+
+    return calculate_mathematical_expression(split_expression[0], split_expression[1], split_expression[2])
--- Year 3/Introduction To Computer Science/Assignments/Ex02/largest_and_smallest.py
+++ Year 3/Introduction To Computer Science/Assignments/Ex02/largest_and_smallest.py
@@ -7,18 +7,18 @@
 # WEB PAGES I USED: w3schools, docs.python.org
 # NOTES: ...
 #################################################################
-#My test cases for check_largest_and_smallest are checking negative numbers and decimals
+# My test cases for check_largest_and_smallest are checking negative numbers and decimals
 
 
-#Gets the maximal and minimal number from three numbers without using max, min or sort.
+# Gets the maximal and minimal number from three numbers without using max, min or sort.
 def largest_and_smallest(first_number, second_number, third_number):
-    #Select largest from the possible combinations ((1,2),(1,3),(2,3))
+    # Select largest from the possible combinations ((1,2),(1,3),(2,3))
     if first_number >= second_number and first_number >= third_number:
         largest_number = first_number
     else:
         largest_number = (second_number if second_number >= third_number else third_number)
 
-    #Select smallest from the possible combinations ((1,2),(1,3),(2,3))
+    # Select smallest from the possible combinations ((1,2),(1,3),(2,3))
     if first_number <= second_number and first_number <= third_number:
         smallest_number = first_number
     else:
@@ -30,23 +30,26 @@ def check_largest_and_smallest():
     any_false_calculation = False
     if not scenario_is_correct_largest_and_smallest(17, 1, 16, 17, 1):
         any_false_calculation = True
+
     if not scenario_is_correct_largest_and_smallest(1, 17, 6, 17, 1):
         any_false_calculation = True
+
     if not scenario_is_correct_largest_and_smallest(1, 1, 2, 2, 1):
         any_false_calculation = True
-    #Checking Negative numbers.
+
+    # Checking Negative numbers.
     if not scenario_is_correct_largest_and_smallest(-1, 0, -3, 0, -3):
         any_false_calculation = True
+
     # Checking decimals
     if not scenario_is_correct_largest_and_smallest(2.25, 2.5, 2.75, 2.75, 2.25):
         any_false_calculation = True
+
     return not any_false_calculation
 
 
-
-#Single Scenario check for largest and smallest (to reduce duplications).
-def scenario_is_correct_largest_and_smallest(first_number, second_number, third_number, expected_largest, expected_smallest):
+# Single Scenario check for largest and smallest (to reduce duplications).
+def scenario_is_correct_largest_and_smallest(first_number, second_number, third_number, expected_largest,
+                                             expected_smallest):
     largest, smallest = largest_and_smallest(first_number, second_number, third_number)
-    if largest != expected_largest or smallest != expected_smallest:
-        return False
-    return True
+    return largest != expected_largest or smallest != expected_smallest
