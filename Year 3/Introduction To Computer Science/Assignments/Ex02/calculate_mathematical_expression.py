cbf29048d1f9a5a9a68f35835cd0d650a913cc72
Noa <noait2612@gmail.com>
2025-10-28 17:23:01 +0200

    ex02 in intro

---
+++ Year 3/Introduction To Computer Science/Assignments/Ex02/calculate_mathematical_expression.py
@@ -0,0 +1,31 @@
+#################################################################
+# FILE : math_print.py
+# WRITER : your_name , your_login , your_id
+# EXERCISE : intro2cs ex2 2025
+# DESCRIPTION: A simple program that provides my secret key.
+# STUDENTS I DISCUSSED THE EXERCISE WITH: -
+# WEB PAGES I USED: w3schools, docs.python.org
+# NOTES: ...
+#################################################################
+#Gets two numbers and one mathematical operation and returns the mathematical operation result on them.
+def calculate_mathematical_expression(first_number, second_number, operation):
+    if operation == "+":
+        return first_number + second_number
+    if operation == "*":
+        return first_number * second_number
+    if operation == "-":
+        return first_number-second_number
+    if operation == ":" and second_number != 0:
+        return first_number/second_number
+    #This else case catches not allowed operation or allowed operation and invalid action (like diving zero).
+    else:
+        return None
+
+
+"""Gets an expression from the format 'number operation number', split by format
+ and return using calculate_mathematical_expression the operation result"""
+def calculate_from_string(expression):
+        split_expression = expression.split(' ')
+        if len(split_expression) != 3:
+            return None
+        return calculate_mathematical_expression(split_expression[0], split_expression[1], split_expression[2])
---
+++ Year 3/Introduction To Computer Science/Assignments/Ex02/temperature.py
@@ -0,0 +1,26 @@
+#################################################################
+# FILE : math_print.py
+# WRITER : your_name , your_login , your_id
+# EXERCISE : intro2cs ex2 2025
+# DESCRIPTION: A simple program that provides my secret key.
+# STUDENTS I DISCUSSED THE EXERCISE WITH: -
+# WEB PAGES I USED: w3schools, docs.python.org
+# NOTES: ...
+#################################################################
+#Checking if each day is over given threshold temperature and check if we have any pair from the combination ((1,2),(1,3),(2,3))
+def is_vormir_safe(threshold_temperature, first_day_temperature, second_day_temperature, third_day_temperature):
+    is_first_day_over_threshold_temperature = is_over_threshold_temperature(threshold_temperature, first_day_temperature)
+    is_second_day_over_threshold_temperature = is_over_threshold_temperature(threshold_temperature,
+                                                                            second_day_temperature)
+    is_third_day_over_threshold_temperature = is_over_threshold_temperature(threshold_temperature,
+                                                                             third_day_temperature)
+    if is_first_day_over_threshold_temperature and (is_second_day_over_threshold_temperature or is_third_day_over_threshold_temperature):
+        return True
+    elif is_second_day_over_threshold_temperature and is_third_day_over_threshold_temperature:
+        return True
+    else:
+        return False
+
+
+def is_over_threshold_temperature(threshold_temperature, temperature):
+    return temperature > threshold_temperature
